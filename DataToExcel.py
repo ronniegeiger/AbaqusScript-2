105d40f7aee5df4bb5f5cf2c283a46c5def89f1c
JasonGeng <1198894714@qq.com>
2022-05-08 16:12:35 +0800

    pre

--- DataToExcel.py
+++ DataToExcel.py
@@ -2,7 +2,7 @@ import openpyxl
 import string
 file_name = 'Data.txt'
 with open(file_name,'r') as DA:
-    Data=DA.read().strip()
-# Pkeys=str.split(Data[0],' ')
-# print(Pkeys)
-print(Data[1])
+    # Data=DA.read().strip()
+    Data=DA.readlines()
+    Pkeys=str.split(Data[0],' ')
+
